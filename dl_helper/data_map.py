fe246b6ee162d2c5ba84146f05c0ea8fde78b88c
lhiqwj173 <lhssqtz@gmail.com>
2024-05-13 16:44:01 +0800

    data map

--- dl_helper/data_map.py
+++ dl_helper/data_map.py
@@ -7,4 +7,5 @@ DATA_MAP = {
     'pred_5_pass_100_y_3_bd_2024_05_01_dr_7@2@3_th_96_s_BTCFDUSD_t_samepaper.7z': "20240513_002.7z",
     'pred_5_pass_100_y_3_bd_2024_05_01_dr_7@2@3_th_96_s_BTCUSDT@ETHUSDT_t_samepaper.7z': "20240513_003.7z",
     'pred_5_pass_100_y_3_bd_2024_05_01_dr_7@2@3_th_96_s_BTCFDUSD@ETHFDUSD_t_samepaper.7z': "20240513_004.7z",
+    'pred_5_pass_100_y_3_bd_2024_05_01_dr_7@2@3_th_96_s_BTCFDUSD@BTCUSDT_t_samepaper.7z': "20240513_005.7z",
 }
