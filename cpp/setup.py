44af318c1bde04e1118aaf54194d305df7000d65
lhiqwj173 <lhssqtz@gmail.com>
2024-05-21 05:18:26 +0800

    适配 linux

--- cpp/setup.py
+++ cpp/setup.py
@@ -2,13 +2,20 @@ import os
 import pybind11
 import numpy as np
 from setuptools import setup, Extension
+import platform
+
+extra_compile_args = []
+if platform.system() == 'Windows':
+    extra_compile_args.append('/std:c++17')
+else:
+    extra_compile_args.append('-std=c++17')
 
 cpp_ext_module = Extension(
     'cpp_ext',
     sources=['cpp_ext.cpp'],
     include_dirs=[pybind11.get_include(), np.get_include()],
     language='c++',
-    extra_compile_args=['/std:c++17']
+    extra_compile_args=extra_compile_args
 )
 
 setup(
