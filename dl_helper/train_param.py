0de1f953222bd90945c2ae22103a0f4d8939629b
lhiqwj173 <lhssqtz@gmail.com>
2024-05-26 05:33:50 +0800

    0 workers

--- dl_helper/train_param.py
+++ dl_helper/train_param.py
@@ -82,6 +82,7 @@ class Params:
 
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
   workers = int(multiprocessing.cpu_count())
+  workers = 0
 
   #############################
   # 训练超参数
