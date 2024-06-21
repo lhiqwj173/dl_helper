6e05bf44ae44da7283898de3b91903dcecd1635c
lhiqwj173 <lhssqtz@gmail.com>
2024-06-22 05:33:18 +0800

    debug

--- dl_helper/tracker.py
+++ dl_helper/tracker.py
@@ -7,6 +7,28 @@ class tracker():
         self.params = params
         self.trader = trader
 
+        # 训练损失
+        self.train_losses = []
+        self.val_losses = []
+        self.test_losses = []
+
+        if params.classify: 
+            # 分类模型 acc
+            self.train_acc = []
+            self.val_acc = []
+            self.test_acc = []
+
+        else:
+            # 回归模型 r2
+            self.train_r2s = [[]]
+            self.val_r2s = [[]]
+            self.test_r2s = [[]]
+
+        # 训练学习率
+        self.lr = []
+
+        
+
     def track(self, output, target, loss):
         pass
 
