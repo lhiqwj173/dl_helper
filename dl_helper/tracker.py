865cfaec9967ea2bd21427e42373be6ca5d3f144
lhiqwj173 <lhssqtz@gmail.com>
2024-06-22 06:10:40 +0800

    debug

--- dl_helper/tracker.py
+++ dl_helper/tracker.py
@@ -1,35 +1,40 @@
 """
 用于追踪训练过程评价指标
 """
+import time
 
 class tracker():
     def __init__(self, params, trader):
+        self.begin_time = time.time()
         self.params = params
         self.trader = trader
 
-        # 训练损失
-        self.train_losses = []
-        self.val_losses = []
-        self.test_losses = []
+        # 最终数据
+        self.data = {}
+        # 计算变量
+        self.temp = {}
+        for i in ['train', 'val', 'test']:
+            # 训练损失
+            self.data[f'{i}_loss'] = []
 
-        if params.classify: 
-            # 分类模型 acc
-            self.train_acc = []
-            self.val_acc = []
-            self.test_acc = []
+            if params.classify: 
+                # 分类模型 acc
+                self.data[f'{i}_acc'] = []
 
-        else:
-            # 回归模型 r2
-            self.train_r2s = [[]]
-            self.val_r2s = [[]]
-            self.test_r2s = [[]]
+                # 暂时只使用加权f1
+                self.data[f'{i}_f1'] = []
+            else:
+                # 回归模型 r2
+                # 暂时只使用平均r2
+                sself.data[f'{i}_r2'] = []
 
         # 训练学习率
-        self.lr = []
+        self.data['lr'] = []
 
-        
 
-    def track(self, output, target, loss):
+
+
+    def track(self, output, target, loss, _type):
         pass
 
     def plot(self):
