6fc356558f89fa7bb737338fa92a4b3ec9479bf4
lhiqwj173 <lhssqtz@gmail.com>
2024-06-24 19:28:12 +0800

    debug

--- dl_helper/trainer.py
+++ dl_helper/trainer.py
@@ -92,6 +92,7 @@ def test_fn(index, params, model, test_data, trainer):
             output = model(data)
 
 def run_fn(index, lock, num_processes, test):
+    print(f'run_fn {index}')
 
     ###########################################
     # 1. 训练/验证
