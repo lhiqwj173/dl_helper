f86efbed00d79e588b2b8bd70cbbbd04e53b0802
lhiqwj173 <lhssqtz@gmail.com>
2024-06-24 19:11:49 +0800

    DEBUG

--- dl_helper/trainer.py
+++ dl_helper/trainer.py
@@ -94,7 +94,7 @@ def run_fn(index, lock, num_processes, test):
     ###########################################
     # 1. 训练/验证
     ###########################################
-    # 调整训练参数
+    trainer.print('调整训练参数...')
     params = test.get_param()
     # 调整batch_size, 多gpu时的batch_size指的是每个gpu的batch_size
     if num_processes == 2:
@@ -104,7 +104,7 @@ def run_fn(index, lock, num_processes, test):
     if num_processes > 0:
         params.learning_rate *= num_processes
 
-    # 创建训练器
+    trainer.print('创建训练器...')
     if num_processes == 8:
         trainer = train_tpu(params, index, lock)
     else:
