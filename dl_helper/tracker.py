ce090225ab62732de710920de5489b5956a7ef10
lhiqwj173 <lhssqtz@gmail.com>
2024-06-29 10:26:27 +0800

    debug

--- dl_helper/tracker.py
+++ dl_helper/tracker.py
@@ -164,7 +164,7 @@ class Tracker():
         self.printer.print('sync track...')
         tensor_loss = torch.tensor(float(loss.item()), device=target.device)
         self.printer.print(f'device: {target.device}')
-        self.printer.print(f'loss: {loss} type: {type(loss)} devide: {loss.device}')
+        self.printer.print(f'loss: {loss} type: {type(loss)} devide: {loss.device} shape: {loss.shape}')
         self.printer.print(f'loss: {tensor_loss} type: {type(tensor_loss)}')
         self.printer.print(f'target: {target}')
         self.printer.print(f'predict: {predict}')
