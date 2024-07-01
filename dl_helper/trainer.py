447f798fc24c1f1b34953a57de81f934094335ed
lhiqwj173 <lhssqtz@gmail.com>
2024-07-01 10:09:42 +0800

    debug

--- dl_helper/trainer.py
+++ dl_helper/trainer.py
@@ -438,7 +438,7 @@ def run_fn_1(lock, num_processes, test_class, args, kwargs, train_param={}, mode
     p = printer(lock, accelerator)
     
     # 检查下载tg训练文件
-    if not params.debug and accelerator.is_local_main_process:
+    if (not params.debug) and accelerator.is_local_main_process:
         tg_download(
             ses,
             f'{params.train_title}.7z',
