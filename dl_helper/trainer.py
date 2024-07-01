b28d1a6cac4802d241d361c72864e9fc6679cc03
lhiqwj173 <lhssqtz@gmail.com>
2024-07-01 10:42:18 +0800

    debug

--- dl_helper/trainer.py
+++ dl_helper/trainer.py
@@ -459,7 +459,6 @@ def run_fn_1(lock, num_processes, test_class, args, kwargs, train_param={}, mode
     train_loader = torch.utils.data.DataLoader(list(range(64*10)), batch_size=2)
     train_loader = accelerator.prepare(train_loader)
     active_dataloader = skip_first_batches(train_loader, 2)
-    accelerator.wait_for_everyone()
     for i in (active_dataloader):
         p.print(i)
     accelerator.wait_for_everyone()
