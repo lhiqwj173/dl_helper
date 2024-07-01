618b44ce3d7826890a54674d09661b2e6d3a20cb
lhiqwj173 <lhssqtz@gmail.com>
2024-07-01 14:24:59 +0800

    debug

--- dl_helper/acc/data_loader.py
+++ dl_helper/acc/data_loader.py
@@ -175,7 +175,6 @@ def skip_first_batches_0(dataloader, num_batches=0):
     return dataloader
 
 def skip_first_batches(dataloader, num_batches):
-    print(f'skip_first_batches {num_batches}')
     it = iter(dataloader)
     next(itertools.islice(it, num_batches, num_batches), None)  # 跳过前 n 个元素
     return it
