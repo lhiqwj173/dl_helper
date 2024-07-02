2f02048248745fd01c779f1fe1f1ba2fd8d0640f
lhiqwj173 <lhssqtz@gmail.com>
2024-07-02 11:03:46 +0800

    debug

--- dl_helper/tests/test_resnet.py
+++ dl_helper/tests/test_resnet.py
@@ -49,7 +49,7 @@ class test(test_base):
         num_classes = 3
 
         # for debug
-        num_samples = 64 * 3
+        num_samples = 10000
 
         data = torch.randn(num_samples, 3, 64, 64)
         target = torch.randint(0, num_classes, (num_samples,))
