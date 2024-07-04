9d3619ad570f4b2b2859ddfdbe17a770560afb04
lhiqwj173 <lhssqtz@gmail.com>
2024-07-04 12:52:15 +0800

    debug

--- dl_helper/trainer.py
+++ dl_helper/trainer.py
@@ -695,12 +695,6 @@ def run_fn_xla(index, lock, num_processes, test_class, args, kwargs, train_param
     train_loader = test.get_data('train', params, get_data_sampler)
     val_loader = test.get_data('val', params, get_data_sampler)
 
-    batch = next(iter(train_loader))
-    xm.master_print(f'batch shape: {batch[0].shape}')
-    xm.master_print(f'batch shape: {batch[1].shape}')
-    xm.master_print(f'batch shape: {batch[2].shape}')
-    raise
-
     xm.rendezvous("init train_loader")# mark_step
     xm.master_print(f'dataset length: {len(train_loader.dataset)}')
     xm.master_print(f'dataloader length: {len(train_loader)}')
@@ -713,6 +707,12 @@ def run_fn_xla(index, lock, num_processes, test_class, args, kwargs, train_param
 
     train_loader = pl.MpDeviceLoader(train_loader, device)
     val_loader = pl.MpDeviceLoader(val_loader, device)
+
+    batch = next(iter(train_loader))
+    xm.master_print(f'batch shape: {batch[0].shape}')
+    xm.master_print(f'batch shape: {batch[1].shape}')
+    xm.master_print(f'batch shape: {batch[2].shape}')
+    raise
 
     if None is model:
         model = test.get_model()
