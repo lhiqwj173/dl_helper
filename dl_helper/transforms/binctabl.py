c8ea54dab750dba54f5531e5474bb2d84b6762a8
lhiqwj173 <lhssqtz@gmail.com>
2024-07-06 05:22:19 +0800

    debug

--- dl_helper/transforms/binctabl.py
+++ dl_helper/transforms/binctabl.py
@@ -18,7 +18,8 @@ class transform():
         self.batch_size = param.batch_size
 
         # 用于random_mask_row
-        self.rand_cols = torch.randint(0, 1, (self.batch_size, self.time_length), device=self.device)
+        # self.rand_cols = torch.randint(0, 1, (self.batch_size, self.time_length), device=self.device)
+        self.rand_cols = torch.zeros(self.batch_size, self.time_length, device=self.device)
         print(self.rand_cols.dtype)
         # if not tpu_available():
         #     self.rand_cols = torch.zeros(self.batch_size, self.time_length, device=self.device, dtype=torch.int64)
