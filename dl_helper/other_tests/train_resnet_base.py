ed21728ebad2653b949c92856296545b3fb4ef7a
lhiqwj173 <lhssqtz@gmail.com>
2024-07-09 07:25:03 +0800

    debug

--- dl_helper/other_tests/train_resnet_base.py
+++ dl_helper/other_tests/train_resnet_base.py
@@ -13,6 +13,18 @@ import torchvision
 import torch.optim as optim
 import torch.nn as nn
 
+def get_data_sampler(data_set, _type):
+    train_sampler = None
+    if xm.xrt_world_size() > 1:
+        train_sampler = torch.utils.data.distributed.DistributedSampler(
+            data_set,
+            num_replicas=xm.xrt_world_size(),
+            rank=xm.get_ordinal(),
+            shuffle=True if _type == 'train' else False
+            )
+
+    return train_sampler
+
 def report_memory_usage(msg=''):
   if xm.is_master_ordinal():
     memory_usage = psutil.virtual_memory()
@@ -20,17 +32,40 @@ def report_memory_usage(msg=''):
 
 class TrainResNetBase():
 
-  def __init__(self, batch_size=128):
+  def __init__(self, batch_size, normal_data=False):
+    self.batch_size = batch_size
+    self.normal_data = normal_data
+
     self.img_dim = 224
-    self.batch_size = 128
     self.num_epochs = 50
     self.train_dataset_len = 12000  # Roughly the size of Imagenet dataset.
-    # For the purpose of this example, we are going to use fake data.
-    train_loader = xu.SampleGenerator(
-        data=(torch.zeros(self.batch_size, 3, self.img_dim, self.img_dim),
-              torch.zeros(self.batch_size, dtype=torch.int64)),
-        sample_count=self.train_dataset_len // self.batch_size //
-        xr.world_size())
+    
+    if not self.normal_data:
+      train_loader = xu.SampleGenerator(
+          data=(torch.zeros(self.batch_size, 3, self.img_dim, self.img_dim),
+                torch.zeros(self.batch_size, dtype=torch.int64)),
+          sample_count=self.train_dataset_len // self.batch_size //
+          xr.world_size())
+    else:
+      data = torch.randn(self.train_dataset_len, 3, self.img_dim, self.img_dim)
+      target = torch.zeros(self.train_dataset_len, dtype=torch.int64)
+      dataset = torch.utils.data.TensorDataset(data, target)
+
+      train_sampler = None
+      if xm.xrt_world_size() > 1:
+          train_sampler = torch.utils.data.distributed.DistributedSampler(
+              dataset,
+              num_replicas=xm.xrt_world_size(),
+              rank=xm.get_ordinal(),
+              )
+
+      # 创建数据加载器
+      loader = torch.utils.data.DataLoader(
+          dataset,
+          batch_size=self.batch_size,
+          sampler=train_sampler,
+          drop_last=True,
+      )
 
     self.device = torch_xla.device()
     self.train_device_loader = pl.MpDeviceLoader(train_loader, self.device)
@@ -66,11 +101,15 @@ class TrainResNetBase():
 
 if __name__ == '__main__':
   batch_size = 128
+  normal_data=False
 
   args = sys.argv
   for i in range(1, len(args)):
     if args[i] == '--batch_size':
       batch_size = int(args[i + 1])
+    
+    elif args[i] == '--normal_data':
+      normal_data = True
 
-  base = TrainResNetBase(batch_size)
+  base = TrainResNetBase(batch_size, normal_data)
   base.start_training()
