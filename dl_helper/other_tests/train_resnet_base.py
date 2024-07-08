ac95620e7e0b34339137d67fd24cf101ce87ce94
lhiqwj173 <lhssqtz@gmail.com>
2024-07-09 06:18:44 +0800

    debug

--- dl_helper/other_tests/train_resnet_base.py
+++ dl_helper/other_tests/train_resnet_base.py
@@ -12,13 +12,16 @@ import torchvision
 import torch.optim as optim
 import torch.nn as nn
 
+def report_memory_usage(msg=''):
+  if xm.is_main_process():
+    memory_usage = psutil.virtual_memory()
+    print(f"{msg} CPU 内存占用：{memory_usage.percent}% ({memory_usage.used/1024**3:.3f}GB/{memory_usage.total/1024**3:.3f}GB)")
 
 class TrainResNetBase():
 
   def __init__(self):
     self.img_dim = 224
     self.batch_size = 128
-    self.num_steps = 300
     self.num_epochs = 1
     self.train_dataset_len = 1200000  # Roughly the size of Imagenet dataset.
     # For the purpose of this example, we are going to use fake data.
@@ -34,35 +37,28 @@ class TrainResNetBase():
     self.optimizer = optim.SGD(self.model.parameters(), weight_decay=1e-4)
     self.loss_fn = nn.CrossEntropyLoss()
 
-  def _train_update(self, step, loss, tracker, epoch):
-    print(f'epoch: {epoch}, step: {step}, loss: {loss}, rate: {tracker.rate()}')
-
   def run_optimizer(self):
     self.optimizer.step()
 
   def train_loop_fn(self, loader, epoch):
-    tracker = xm.RateTracker()
     self.model.train()
-    loader = itertools.islice(loader, self.num_steps)
     for step, (data, target) in enumerate(loader):
       self.optimizer.zero_grad()
       output = self.model(data)
       loss = self.loss_fn(output, target)
       loss.backward()
       self.run_optimizer()
-      tracker.add(self.batch_size)
-      if step % 10 == 0:
-        xm.add_step_closure(
-            self._train_update, args=(step, loss, tracker, epoch))
 
   def start_training(self):
+    xm.master_print('train begin {}'.format(
+        time.strftime('%l:%M%p %Z on %b %d, %Y')))
 
     for epoch in range(1, self.num_epochs + 1):
-      xm.master_print('Epoch {} train begin {}'.format(
-          epoch, time.strftime('%l:%M%p %Z on %b %d, %Y')))
       self.train_loop_fn(self.train_device_loader, epoch)
-      xm.master_print('Epoch {} train end {}'.format(
-          epoch, time.strftime('%l:%M%p %Z on %b %d, %Y')))
+      report_memory_usage(f'epoch {epoch}')
+
+    xm.master_print('train end {}'.format(
+        time.strftime('%l:%M%p %Z on %b %d, %Y')))
     xm.wait_device_ops()
 
 
