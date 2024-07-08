bdb8730b4c5306baf085240ec4e63e81a6189a32
lhiqwj173 <lhssqtz@gmail.com>
2024-07-09 07:03:04 +0800

    debug

--- dl_helper/other_tests/train_resnet_base.py
+++ dl_helper/other_tests/train_resnet_base.py
@@ -3,7 +3,7 @@ import torch_xla.utils.utils as xu
 import torch_xla.core.xla_model as xm
 import torch_xla.distributed.parallel_loader as pl
 
-import time
+import time, psutil
 import itertools
 
 import torch
