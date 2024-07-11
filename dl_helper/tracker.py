fa9a0284f89c566982962b519cd4ad115327cf19
lhiqwj173 <lhssqtz@gmail.com>
2024-07-12 06:26:07 +0800

    debug

--- dl_helper/tracker.py
+++ dl_helper/tracker.py
@@ -359,8 +359,9 @@ class Tracker():
 
             pic_file = os.path.join(params.root, f"{params.train_title}.png")
             plt.savefig(pic_file)
+            # self.printer.print(f'plot done: {pic_file}')
 
-        self.printer.print(f'plot done: {pic_file}')
+        self.printer.print(f'plot done')
         self.accelerator.wait_for_everyone()
 
     def state_dict(self):
