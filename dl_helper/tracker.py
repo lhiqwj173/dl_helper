5f4d49c2fba00579f13b2d144685c6bab662a606
lhiqwj173 <lhssqtz@gmail.com>
2024-07-13 11:49:27 +0800

    debug

--- dl_helper/tracker.py
+++ dl_helper/tracker.py
@@ -302,8 +302,7 @@ class Tracker():
             # 标准化数据，nan补气数据
             data = {}
             for i in self.data:
-                self.printer.print(i, type(self.data[i]))
-                data[i] = self.data[i].cpu().tolist() if not None is self.data[i] else []
+                data[i] = [] if None is self.data[i] else copy.deepcopy(self.data[i]) if isinstance(self.data[i], list) else self.data[i].cpu().tolist()
 
                 if 'test' in i:
                     data[i] = [data[i][-1]] * epochs if len(data[i]) else []
