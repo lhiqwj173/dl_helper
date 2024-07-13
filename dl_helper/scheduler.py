fce5c194c3166a07ccfdb264a2c2f1353ef693c5
lhiqwj173 <lhssqtz@gmail.com>
2024-07-13 08:00:13 +0800

    ReduceLR_slow_loss 优化成torch

--- dl_helper/scheduler.py
+++ dl_helper/scheduler.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from dl_helper.train_param import tpu_available
+if tpu_available():
+    import torch_xla.core.xla_model as xm
 
 
 # 当训练的损失序列区域平缓时，减低学习率
@@ -20,17 +23,32 @@ class ReduceLR_slow_loss():
             self.wait -= 1
             return
 
-        # 计算损失均线，ma=self.patience
-        # 均线变动率 大于 min_pct 则减少学习率
-        loss = pd.DataFrame({'loss': array_loss}).dropna()
-        if len(loss) < self.patience+1:
+        # # 计算损失均线，ma=self.patience
+        # # 均线变动率 大于 min_pct 则减少学习率
+        # loss = pd.DataFrame({'loss': array_loss}).dropna()
+        # if len(loss) < self.patience+1:
+        #     return
+        # loss['ma'] = loss['loss'].rolling(self.patience).mean()
+        # loss['pct'] = loss['ma'].pct_change()
+        # loss['match'] = loss['pct']>=self.min_pct
+        # if loss.iloc[-1]['match']:
+        #     self._reduce_lr()
+        # elif self.debug:
+        #     print('pass')
+
+        # 改用torch
+        if array_loss.size()[0] < self.patience+1:
             return
+        # 计算损失均线
+        loss_ma = array_loss.unfold(dimension=0, size=self.patience, step=1).mean(dim=1)
+        # 计算均线变动率
+        loss_pct_change = loss_ma[1:] / loss_ma[:-1] - 1
+        # 判断是否满足减少学习率条件
+        match = (loss_pct_change >= self.min_pct)
 
-        loss['ma'] = loss['loss'].rolling(self.patience).mean()
-        loss['pct'] = loss['ma'].pct_change()
-        loss['match'] = loss['pct']>=self.min_pct
-
-        if loss.iloc[-1]['match']:
+        if tpu_available():
+            xm.mark_step()
+        if match[-1].item():
             self._reduce_lr()
         elif self.debug:
             print('pass')
