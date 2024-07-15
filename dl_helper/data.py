9886378b3b7cf84ec950694a5cf163262352b5a4
lhiqwj173 <lhssqtz@gmail.com>
2024-07-16 07:49:07 +0800

    debug

--- dl_helper/data.py
+++ dl_helper/data.py
@@ -850,13 +850,17 @@ class DistributedSampler(Sampler):
 
         self.mini_epoch_indices_ramain -= 1
         self.dataset.load_data()
+        debug('dataset.load_data done')
 
         # 同步数据长度
         # data_length = torch.tensor([len(self.dataset)], device=self.accelerator.device)
         data_length = torch.tensor(len(self.dataset), device=self.accelerator.device)
+        debug(f'data_length 0: {data_length}')
         self.accelerator.wait_for_everyone()
         data_length = self.accelerator.gather_for_metrics(data_length)
+        debug(f'data_length 1: {data_length}')
         data_length = torch.min(data_length)
+        debug(f'data_length 2: {data_length}')
 
         if self.shuffle:
             indices = list(torch.randperm(data_length))
