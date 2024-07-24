5a164c28a6ef94e329aaa39d30f2be5c12fc394d
lhiqwj173 <lhssqtz@gmail.com>
2024-07-24 08:32:01 +0800

    debug

--- dl_helper/data.py
+++ dl_helper/data.py
@@ -480,6 +480,7 @@ class Dataset_cahce(torch.utils.data.Dataset):
 
     def init_data_thread_start(self, mini_epoch_file_indices, mini_dataset_length, mini_epoch, world_size, rank):
         debug(f'init_data_thread_start {rank} {self.type}')
+        self.producer_thread_stop = False
         producer_thread = threading.Thread(target=self._init_data, args=(mini_epoch_file_indices, mini_dataset_length, mini_epoch, world_size, rank))
         producer_thread.start()
 
@@ -523,6 +524,7 @@ class Dataset_cahce(torch.utils.data.Dataset):
                 except:
                     # 检查是否需要暂停
                     if self.producer_thread_stop:
+                        log(f"{self.type} producer_thread_stop:{self:producer_thread_stop}")
                         break
 
             debug(f'put {i} mini_epoch data_map, ramin:{self.q.qsize()} full:{self.q.full()}')
