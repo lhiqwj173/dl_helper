50aced4624d6ca455583fde93e71ed37ac0747f9
lhiqwj173 <lhssqtz@gmail.com>
2024-07-25 12:34:39 +0800

    DEBUG

--- dl_helper/tracker.py
+++ dl_helper/tracker.py
@@ -326,9 +326,9 @@ class Tracker():
         if self.params.classify and self.accelerator.is_main_process and _type not in self.label_count_done:
             debug('统计 label_counts')
             if _type not in self.label_counts:
-                self.label_counts[_type] = torch.bincount(labels, minlength=self.params.y_n)
+                self.label_counts[_type] = torch.bincount(target, minlength=self.params.y_n)
             else:
-                self.label_counts[_type] += torch.bincount(labels, minlength=self.params.y_n)
+                self.label_counts[_type] += torch.bincount(target, minlength=self.params.y_n)
             debug('统计 label_counts done')
 
         self.track_update = _type
