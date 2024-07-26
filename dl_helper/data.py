c85141971c0ff964aed3430878d8a30c58bc834c
lhiqwj173 <lhssqtz@gmail.com>
2024-07-26 19:45:25 +0800

    debug

--- dl_helper/data.py
+++ dl_helper/data.py
@@ -441,6 +441,7 @@ class Dataset_cahce(torch.utils.data.Dataset):
 
         # 针对按照文件夹（train/val/test）分配的数据集
         folder_data_path = os.path.join(data_path, self.type)
+        debug(folder_data_path)
         if os.path.exists(folder_data_path):
             info(f'使用文件夹方式读取数据 {self.type}: {folder_data_path}')
             self.files = sorted([i for i in os.listdir(folder_data_path)])
