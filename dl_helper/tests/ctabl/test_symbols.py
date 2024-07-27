7ee0ae9bea3b6e21944f268574a8e81ba3506d44
lhiqwj173 <lhssqtz@gmail.com>
2024-07-27 16:49:05 +0800

    debug

--- dl_helper/tests/ctabl/test_symbols.py
+++ dl_helper/tests/ctabl/test_symbols.py
@@ -55,7 +55,8 @@ class test(test_base):
 
         batch_n = 16 * 4# 7.9GB
         batch_n = 16 * 8# 7.8 - 8GB
-        batch_n = 16 * (2**6)
+        batch_n = 16 * (2**6) # 11.6 - 11.9GiB
+        batch_n = 16 * (2**8)
 
         yfunc = functools.partial(yfunc, 4e-05)
 
