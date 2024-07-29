ab707eea009e1d1eb9d9479b3c95322c34929c1a
lhiqwj173 <lhssqtz@gmail.com>
2024-07-30 07:25:49 +0800

    debug

--- envs/train.py
+++ envs/train.py
@@ -21,11 +21,11 @@ try:
         # !git clone https://github.com/lhiqwj173/accelerate.git
         # !cd accelerate && git checkout fix-save_state-bug-with-MpDeviceLoaderWrapper-object && pip install .
         cmd = "pip uninstall accelerate -y"
-        subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL)
+        subprocess.call(cmd, shell=True)#, stdout=subprocess.DEVNULL)
         cmd = "git clone https://github.com/lhiqwj173/accelerate.git"
-        subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL)
+        subprocess.call(cmd, shell=True)#, stdout=subprocess.DEVNULL)
         cmd = 'cd accelerate && git checkout fix-save_state-bug-with-MpDeviceLoaderWrapper-object && pip install .'
-        subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL)
+        subprocess.call(cmd, shell=True)#, stdout=subprocess.DEVNULL)
     
 except importlib.metadata.PackageNotFoundError:
     print(f"The package {package_name} is not installed.")
@@ -33,35 +33,35 @@ except importlib.metadata.PackageNotFoundError:
     # !git clone https://github.com/lhiqwj173/accelerate.git
     # !cd accelerate && git checkout fix-save_state-bug-with-MpDeviceLoaderWrapper-object && pip install .
     cmd = 'git clone https://github.com/lhiqwj173/accelerate.git'
-    subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL)
+    subprocess.call(cmd, shell=True)#, stdout=subprocess.DEVNULL)
     cmd = 'cd accelerate && git checkout fix-save_state-bug-with-MpDeviceLoaderWrapper-object && pip install .'
-    subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL)
+    subprocess.call(cmd, shell=True)#, stdout=subprocess.DEVNULL)
 
 root = r'/kaggle/working/' if ENV==KAGGLE else r'/content/'
 if os.path.exists(os.path.join(root, '3rd')):
     if ENV==KAGGLE:
         #!cd /kaggle/working/3rd/dl_helper && git pull
         cmd = 'cd /kaggle/working/3rd/dl_helper && git pull'
-        subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL)
+        subprocess.call(cmd, shell=True)#, stdout=subprocess.DEVNULL)
     else:
         # !cd /content/3rd/dl_helper && git pull
         cmd = 'cd /content/3rd/dl_helper && git pull'
-        subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL)
+        subprocess.call(cmd, shell=True)#, stdout=subprocess.DEVNULL)
 else:
     # !mkdir 3rd && cd 3rd && git clone https://github.com/lhiqwj173/dl_helper.git
     cmd = 'mkdir 3rd && cd 3rd && git clone https://github.com/lhiqwj173/dl_helper.git'
-    subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL)
+    subprocess.call(cmd, shell=True)#, stdout=subprocess.DEVNULL)
     if ENV==KAGGLE:
         # !cd /kaggle/working/3rd/dl_helper && pip install -e .
         cmd = 'cd /kaggle/working/3rd/dl_helper && pip install -e .'
-        subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL)
+        subprocess.call(cmd, shell=True)#, stdout=subprocess.DEVNULL)
         ##  !cd /kaggle/working/3rd/dl_helper && git checkout 5b178e9 && pip install -e .
         # cmd = 'cd /kaggle/working/3rd/dl_helper && git checkout 5b178e9 && pip install -e .'
-        # subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL)
+        # subprocess.call(cmd, shell=True)#, stdout=subprocess.DEVNULL)
     else:
         # !cd /content/3rd/dl_helper && pip install -e .
         cmd = 'cd /content/3rd/dl_helper && pip install -e .'
-        subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL)
+        subprocess.call(cmd, shell=True)#, stdout=subprocess.DEVNULL)
 
     # !pip install "pandas<2.0.0"
     # !pip install loguru
@@ -85,7 +85,7 @@ else:
             'pip install requests_toolbelt',
             'pip install https://raw.githubusercontent.com/lhiqwj173/dl_helper/master/py_ext-1.0.0.tar.gz',
         ]:
-        subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL)
+        subprocess.call(cmd, shell=True)#, stdout=subprocess.DEVNULL)
 
     sys.path.append(os.path.join(root, '3rd', 'dl_helper'))
     
