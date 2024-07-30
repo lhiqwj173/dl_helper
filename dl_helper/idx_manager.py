79612cedb416b8b6013d677766b601be3dbd7787
lhiqwj173 <lhssqtz@gmail.com>
2024-07-30 10:21:14 +0800

    debug

--- dl_helper/idx_manager.py
+++ dl_helper/idx_manager.py
@@ -24,8 +24,8 @@ def get_idx(train_title):
 
 def run_idx_manager():
     # 定义服务器地址和端口
-    HOST = '127.0.0.1'
-    PORT = 12345
+    HOST = '168.138.158.156'
+    PORT = 12345de ug
 
     # 创建一个 TCP 套接字
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
