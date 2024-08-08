d1c864ed4143e9f555d6084eeee4fac15d6e0c0d
lhiqwj173 <lhssqtz@gmail.com>
2024-08-08 12:46:20 +0800

    test_max_bs

--- dl_helper/tests/ctabl/test_base_t0_max_bs.py
+++ dl_helper/tests/ctabl/test_base_t0_max_bs.py
@@ -13,7 +13,19 @@ from py_ext.tool import log, init_logger
 init_logger('base', level='ERROR')
 
 """
-最大可用batchsize
+- 使用新的标签 
+    价差/最小变单位 >=  1 -上涨 0
+    价差/最小变单位 <= -1 -下跌 1
+    else                -不变 2
+- 验证数据集为训练数据集同期内随机12天的数据，在训练集中排除
+
+数据密度较小，不使用降采样
+
+不使用数据增强
+
+数据增加 172d
+训练:验证:测试
+160:12:6
 """
 
 def yfunc(y):
@@ -28,13 +40,12 @@ class test(test_base):
 
     @classmethod
     def title_base(cls):
-        return 'binctabl_bin_max_bs'
+        return 'binctabl_base_t0'
 
     def __init__(self, *args, target_type=1, lr_scheduler_class='WarmupReduceLROnPlateau', **kwargs):
         super().__init__(*args, **kwargs)
 
         self.lr_scheduler_class = lr_scheduler_class
-        symbols = ['ETHFDUSD', 'ETHUSDT', 'BTCFDUSD', 'BTCUSDT']
 
         classify_idx, targrt_name = 4, '10_target_mid_diff'
         self.y_n = 3
@@ -43,12 +54,12 @@ class test(test_base):
 
         # T: 100, 40, 10, 1
         model_vars = [
-            ((100, 40, 10, 1), 3.8e-7),
-            ((100, 60, 20, 1), 5e-7),
-            ((100, 80, 30, 1), 5e-7),
-            ((100, 100, 40, 1), 3.8e-7),
-            ((100, 120, 50, 1), 3.8e-7),
-            ((100, 140, 60, 1), 3.8e-7),
+            ((100, 40, 10, 1), 5.6e-6),
+            ((100, 60, 20, 1), 9.6e-6),
+            ((100, 80, 30, 1), 1.3e-5),
+            ((100, 100, 40, 1), 5.6e-6),
+            ((100, 120, 50, 1), 4.3e-6),
+            ((100, 140, 60, 1), 5.6e-6),
         ]
         self.model_var = model_vars[0][0]
         self.lr = model_vars[0][1]
@@ -61,7 +72,7 @@ class test(test_base):
             'begin_date': '2024-05-01',
             'data_rate': (8, 3, 1),
             'total_hours': int(24*20),
-            'symbols': '@'.join(symbols),
+            'symbols': '成交量 >= 100w',
             'target': targrt_name,
             'std_mode': '5d'  # 4h/1d/5d
         }
@@ -69,13 +80,10 @@ class test(test_base):
         # 实例化 参数对象
         self.para = Params(
             train_title=title, root=f'./{title}', data_set=f'{data_parm2str(data_parm)}.7z',
-            abs_learning_rate=self.lr, batch_size=64*batch_n, epochs=150,
+            abs_learning_rate=self.lr, batch_size=64*batch_n, epochs=300,
 
             # 学习率衰退延迟
             learning_rate_scheduler_patience=10,
-
-            # 每 5 个样本取一个数据
-            down_freq=5,
 
             # 3分类
             classify=True,
@@ -101,6 +109,7 @@ class test(test_base):
         return transform(device, self.para, 103)
 
 if '__main__' == __name__:
+
     for i in range(10):
         batch_size = 2048 * (2 ** (i+1))
         run(
@@ -108,9 +117,9 @@ if '__main__' == __name__:
             # findbest_lr=True, 
             debug=True,
             mode='cache_data',
-            data_folder=r'/kaggle/input/lh-q-bin-data-20240805',
+            data_folder=r'/kaggle/input/lh-q-t0-data-20240805',
             train_param={
                 'batch_size': batch_size,
             }
         )
-        print(f'test Bin batch_size: {batch_size}')
+        print(f'test T0 batch_size: {batch_size}')
