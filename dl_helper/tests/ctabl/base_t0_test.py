683b1c853be2fe8c1d941fc2755035bc42d94e49
lhiqwj173 <lhssqtz@gmail.com>
2024-08-12 15:24:40 +0800

    debug

--- dl_helper/tests/ctabl/base_t0_test.py
+++ dl_helper/tests/ctabl/base_t0_test.py
@@ -140,7 +140,7 @@ if '__main__' == __name__:
         test, 
         # findbest_lr=True, 
         mode='cache_data',
-        data_folder=r'/kaggle/input/lh-q-t0-data-20240805'
+        data_folder=r'/kaggle/input/lh-q-t0-data-20240805',
         only_predict = True,
         # data_folder=r'Z:\L2_DATA\20240729',
     )
