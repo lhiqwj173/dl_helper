21e5a07d82a4e1b9d879d0ea04b87417068304a6
lhiqwj173 <lhssqtz@gmail.com>
2024-08-17 09:28:49 +0800

    debug

--- dl_helper/tests/ctabl/base_t0_3.py
+++ dl_helper/tests/ctabl/base_t0_3.py
@@ -134,7 +134,7 @@ if '__main__' == __name__:
 
     run(
         test, 
-        findbest_lr=True, 
+        findbest_lr=True,
         amp='fp16',
         mode='cache_data',
         data_folder=r'/kaggle/input/lh-q-t0-data-20240817'
