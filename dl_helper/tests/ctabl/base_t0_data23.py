f33d4b4d85e7d971a1cba4c77a6105a1f28f64eb
lhiqwj173 <lhssqtz@gmail.com>
2024-10-17 22:00:01 +0800

    debug

--- dl_helper/tests/ctabl/base_t0_data23.py
+++ dl_helper/tests/ctabl/base_t0_data23.py
@@ -145,6 +145,7 @@ class test(test_base):
 if '__main__' == __name__:
 
     input_folder = r'/kaggle/input'
+    input_folder = r'C:\Users\lh\Desktop\temp\test_train_data'
 
     data_folder_name = os.listdir(input_folder)[0]
     data_folder = os.path.join(input_folder, data_folder_name)
@@ -167,6 +168,6 @@ if '__main__' == __name__:
         mode='cache_data',
         data_folder=data_folder,
 
-        # debug=True,
-        # idx=0
+        debug=True,
+        idx=0
     )
