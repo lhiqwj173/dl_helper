8442ad640938a56db9ff488f744927c62693ee78
lhiqwj173 <lhssqtz@gmail.com>
2024-10-26 05:31:51 +0800

    debug

--- dl_helper/tests/ctabl/base_t0_data43.py
+++ dl_helper/tests/ctabl/base_t0_data43.py
@@ -113,7 +113,7 @@ class test(test_base):
         # 实例化 参数对象
         self.para = Params(
             train_title=title, root=f'./{title}', data_set=f'{data_parm2str(data_parm)}.7z',
-            batch_size=64*2, epochs=5,
+            batch_size=64*2, epochs=epochs,
 
             # 3分类
             classify=True,
