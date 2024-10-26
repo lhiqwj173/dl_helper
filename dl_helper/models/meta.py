e61e9fc3e697817b8384c91144b8a9bd5dc0b9f2
lhiqwj173 <lhssqtz@gmail.com>
2024-10-26 17:49:57 +0800

    debug

--- dl_helper/models/meta.py
+++ dl_helper/models/meta.py
@@ -2,6 +2,10 @@ import torch
 import torch.nn as nn
 
 class m_meta(nn.Module):
+    @classmethod
+    def model_name(cls):
+        return "meta"
+
     def __init__(self, input_len, y_len):
         super(m_meta, self).__init__()
         self.linear = nn.Linear(input_len, y_len)
