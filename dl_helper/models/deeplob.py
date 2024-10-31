7131905f93b53abb7d82b06f97ed37a7f2724546
lhiqwj173 <lhssqtz@gmail.com>
2024-10-31 17:30:14 +0800

    debug

--- dl_helper/models/deeplob.py
+++ dl_helper/models/deeplob.py
@@ -152,6 +152,67 @@ class m_deeplob_depth(m_deeplob_dropout):
     def model_name(cls):
         return "deeplob_depth"
 
+    def __init__(self, y_len):
+        super().__init__()
+        self.y_len = y_len
+        
+        # convolution blocks
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(1,2), stride=(1,2)),
+            nn.LeakyReLU(negative_slope=0.01),
+            # nn.Tanh(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4,1)),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4,1)),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(32),
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(1,10)),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4,1)),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4,1)),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(32),
+        )
+        
+        # inception moduels
+        self.inp1 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1,1), padding='same'),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,1), padding='same'),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(64),
+        )
+        self.inp2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1,1), padding='same'),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(5,1), padding='same'),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(64),
+        )
+        self.inp3 = nn.Sequential(
+            nn.MaxPool2d((3, 1), stride=(1, 1), padding=(1, 0)),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1,1), padding='same'),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.BatchNorm2d(64),
+        )
+        
+        # lstm layers
+        self.lstm = nn.LSTM(input_size=192, hidden_size=64, num_layers=1, batch_first=True)
+        self.fc1 = nn.Linear(64, self.y_len)
+
+        self.drop_layer1 = nn.Dropout(p=0.2)
+        self.drop_layer2 = nn.Dropout(p=0.2)
+
     def forward(self, x):
         # x = x[:, :, :, :40]
 
@@ -160,7 +221,6 @@ class m_deeplob_depth(m_deeplob_dropout):
         c0 = torch.zeros(1, x.size(0), 64).to(x.device)
     
         x = self.conv1(x)
-        # x = self.conv2(x)
         x = self.conv3(x)
         
         x_inp1 = self.inp1(x)
