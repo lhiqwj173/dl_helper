54b89f5a027c0bd1b5ba027c8a3e66b9521bdf61
lhiqwj173 <lhssqtz@gmail.com>
2024-11-09 09:56:59 +0800

    debug

--- dl_helper/ag_trainer.py
+++ dl_helper/ag_trainer.py
@@ -15,7 +15,7 @@ from py_ext.lzma import compress_folder, decompress
 from py_ext.wechat import send_wx
 from py_ext.alist import alist
 
-logger_ag = logging.getLogger("autogluon")
+# logger_ag = logging.getLogger("autogluon")
 
 def get_gpu_name():
     if 'CUDA_VERSION' in os.environ:
@@ -44,12 +44,12 @@ def compress_update(title, root):
     # 打包文件夹 并 上传到alist
     zip_file = f'{title}.7z'
     compress_folder(root, zip_file, 9, inplace=False)
-    logger_ag.log(20, 'compress_folder done')
+    print('compress_folder done')
 
     # 上传更新到alist
     client = alist(os.environ.get('ALIST_USER'), os.environ.get('ALIST_PWD'))
     client.upload(zip_file, '/ag_train_data/')
-    logger_ag.log(20, 'upload done')
+    print('upload done')
 
 # 共享的标志变量
 stop_flag = False
@@ -60,7 +60,7 @@ def keep_update(title, root):
         time.sleep(600)  # 10分钟
 
         # 执行更新操作
-        logger_ag.log(20, 'compress_update')
+        print('compress_update')
         compress_update(title, root)
 
 kaggle = any(key.startswith("KAGGLE") for key in os.environ.keys())
@@ -115,32 +115,32 @@ def autogluon_train_func(quality='medium', title='', id='id', label='label', use
     thread = threading.Thread(target=keep_update, args=(title, root))
     thread.start()
 
-    logger_ag.log(20, f'读取训练数据({((time.time() - begin_time) / 3600):2f}h)')
+    print(f'读取训练数据({((time.time() - begin_time) / 3600):2f}h)')
     # train_data = TabularDataset(pickle.load(open(os.path.join(train_data_folder, 'train.pkl'), 'rb')))
     train_data = TabularDataset(pd.read_pickle(open(os.path.join(train_data_folder, 'train.pkl'), 'rb')))
     if use_length > 0:
         train_data = train_data.iloc[-int(min(use_length, len(train_data))):].reset_index(drop=True)
-    logger_ag.log(20, f'训练数据长度: {len(train_data)}({((time.time() - begin_time) / 3600):2f}h)')
+    print(f'训练数据长度: {len(train_data)}({((time.time() - begin_time) / 3600):2f}h)')
     # 预处理标签
     if not None is yfunc:
         train_data[label] = train_data[label].apply(yfunc)
 
-    logger_ag.log(20, f'开始训练模型({((time.time() - begin_time) / 3600):2f}h)')
+    print(f'开始训练模型({((time.time() - begin_time) / 3600):2f}h)')
     predictor = TabularPredictor(label=label, eval_metric=mean_class_f1_scorer, verbosity=3, path=ag_data_folder, log_to_file=True, log_file_path=os.path.join(root, 'log.txt'))
     clear_train_data = train_data.drop(columns = [id])
     predictor.fit(clear_train_data, num_gpus=get_gpu_num(), presets=quality, time_limit=int(3600*10.5))
 
-    logger_ag.log(20, f'读取测试数据({((time.time() - begin_time) / 3600):2f}h)')
+    print(f'读取测试数据({((time.time() - begin_time) / 3600):2f}h)')
     # test_data = TabularDataset(pickle.load(open(os.path.join(train_data_folder, 'test.pkl'), 'rb')))
     test_data = TabularDataset(pd.read_pickle(open(os.path.join(train_data_folder, 'test.pkl'), 'rb')))
     if use_length > 0:
         test_data = test_data.iloc[-int(min(use_length, len(test_data))):].reset_index(drop=True)
-    logger_ag.log(20, f'测试数据长度: {len(test_data)}({((time.time() - begin_time) / 3600):2f}h)')
+    print(f'测试数据长度: {len(test_data)}({((time.time() - begin_time) / 3600):2f}h)')
     # 预处理标签
     if not None is yfunc:
         test_data[label] = test_data[label].apply(yfunc)
 
-    logger_ag.log(20, f'储存预测测试数据集({((time.time() - begin_time) / 3600):2f}h)')
+    print(f'储存预测测试数据集({((time.time() - begin_time) / 3600):2f}h)')
     test_to_save = test_data.loc[:, [id, label]].copy()
     predict_proba = predictor.predict_proba(test_data.drop(columns=[id, label]))
     test_to_save[[str(i) for i in range(len(list(predict_proba)))]] = predict_proba.values
@@ -150,7 +150,7 @@ def autogluon_train_func(quality='medium', title='', id='id', label='label', use
     test_end = test_data[id].iloc[-1].split('_')[-1]
     test_to_save.to_csv(os.path.join(root, f'{symbol}_{test_begin}_{test_end}.csv'), index=False)
 
-    logger_ag.log(20, f'评估模型({((time.time() - begin_time) / 3600):2f}h)')
+    print(f'评估模型({((time.time() - begin_time) / 3600):2f}h)')
     leaderboard = predictor.leaderboard(test_data)
     cols = list(leaderboard)
     leaderboard['rank'] = leaderboard['score_val'].rank(ascending=False)
@@ -158,22 +158,22 @@ def autogluon_train_func(quality='medium', title='', id='id', label='label', use
     leaderboard.to_csv(os.path.join(root, f'leaderboard.csv'), index=False)
     output_leaderboard_png(leaderboard, os.path.join(root, f'leaderboard.jpg'))
 
-    logger_ag.log(20, f'停止报告线程({((time.time() - begin_time) / 3600):2f}h)')
+    print(f'停止报告线程({((time.time() - begin_time) / 3600):2f}h)')
     stop_flag = True
     thread.join()
 
-    logger_ag.log(20, f'特征重要性({((time.time() - begin_time) / 3600):2f}h)')
+    print(f'特征重要性({((time.time() - begin_time) / 3600):2f}h)')
     importance = predictor.feature_importance(test_data.drop(columns = [id]), time_limit=3600)
     importance.to_csv(os.path.join(root, f'feature_importance.csv'), index=False)
 
-    logger_ag.log(20, f'压缩更新({((time.time() - begin_time) / 3600):2f}h)')
+    print(f'压缩更新({((time.time() - begin_time) / 3600):2f}h)')
     # 拷贝 训练文件夹
     shutil.copytree(ag_data_folder, os.path.join(root, 'ag'))
     compress_update(title, root)
 
     # 计算耗时
     msg = f'ag训练完成({((time.time() - begin_time) / 3600):2f}h): \n{title}'
-    logger_ag.log(20, msg)
+    print(msg)
     send_wx(msg)
 
 
