d74f228ed23fd67af4b46f7317dccdecd517a054
lhiqwj173 <lhssqtz@gmail.com>
2024-12-05 21:48:58 +0800

    debug

--- dl_helper/rl/net_center.py
+++ dl_helper/rl/net_center.py
@@ -460,9 +460,13 @@ def run_param_center(agent, tau= 0.005):
                                 # 更新计数
                                 update_count += 1
                                 # 更新是否需要验证测试
-                                if update_count % 50000 == 0:
+                                _test_count = 50000
+                                _val_count = 1000
+                                _test_count = 20
+                                _val_count = 10
+                                if update_count % _test_count == 0:
                                     need_test = True
-                                elif update_count % 1000 == 0:
+                                elif update_count % _val_count == 0:
                                     need_val = True
 
                         elif cmd in ['val', 'test']:
