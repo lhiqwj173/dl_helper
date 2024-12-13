5dda11ab643a9106d03fccca3e1a34a8cb960eca
lhiqwj173 <lhssqtz@gmail.com>
2024-12-13 10:40:08 +0800

    debug

--- dl_helper/rl/base.py
+++ dl_helper/rl/base.py
@@ -7,6 +7,7 @@ from py_ext.tool import log, debug, get_log_folder, _get_caller_info
 from py_ext.lzma import compress_folder, decompress
 from py_ext.wechat import wx
 
+from dl_helper.rl.rl_env.lob_env import ILLEGAL_REWARD
 from dl_helper.rl.net_center import get_net_params, send_net_updates, update_model_params, send_val_test_data, check_need_val_test
 from dl_helper.rl.rl_utils import ReplayBuffer, ReplayBufferWaitClose
 from dl_helper.rl.tracker import Tracker
