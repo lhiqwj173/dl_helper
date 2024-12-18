db8f87e3b1c26055eb508b357351e4952bb2bfe4
lhiqwj173 <lhssqtz@gmail.com>
2024-12-18 10:11:54 +0800

    debug

--- dl_helper/tests/rl/run_all.py
+++ dl_helper/tests/rl/run_all.py
@@ -5,6 +5,7 @@ import subprocess
 
 def run(need_clear):
     if need_clear:
+        print('清理 /root/alist_data/rl_learning_process 目录下的所有文件')
         # 清理 /root/alist_data/rl_learning_process 目录下的所有文件
         shutil.rmtree('/root/alist_data/rl_learning_process')
         os.makedirs('/root/alist_data/rl_learning_process')
@@ -12,11 +13,13 @@ def run(need_clear):
     code_folder = r'/root/code/dl_helper'
 
     # 训练服务
+    print('注册训练服务')
     for py_file in ['20241203_dddqn.py', '20241216_per_dqn.py', '20241217_c51.py']:
         path = os.path.join(code_folder, 'dl_helper', 'tests', 'rl', py_file)
         subprocess.run(f'python3 {path} server', check=True)
 
     # 运行服务端
+    print('运行服务端')
     # D:\code\dl_helper\dl_helper\rl\net_center.py
     subprocess.run(f'python3 {code_folder}/dl_helper/rl/net_center.py', check=True)
 
