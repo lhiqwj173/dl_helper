18c62639da467d19de201a5cfefab4688c0e8901
lhiqwj173 <lhssqtz@gmail.com>
2024-12-18 10:14:05 +0800

    debug

--- dl_helper/tests/rl/run_all.py
+++ dl_helper/tests/rl/run_all.py
@@ -16,12 +16,12 @@ def run(need_clear):
     print('注册训练服务')
     for py_file in ['20241203_dddqn.py', '20241216_per_dqn.py', '20241217_c51.py']:
         path = os.path.join(code_folder, 'dl_helper', 'tests', 'rl', py_file)
-        subprocess.run(f'python3 {path} server', check=True)
+        subprocess.run(['python3', path, 'server'], check=True)
 
     # 运行服务端
     print('运行服务端')
     # D:\code\dl_helper\dl_helper\rl\net_center.py
-    subprocess.run(f'python3 {code_folder}/dl_helper/rl/net_center.py', check=True)
+    subprocess.run(['python3', '/root/code/dl_helper/dl_helper/rl/net_center.py'], check=True)
 
 if __name__ == '__main__':
     need_clear = False
