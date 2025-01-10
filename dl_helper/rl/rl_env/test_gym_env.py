30420bc8f825a671c53e305cef77099967493950
lhiqwj173 <lhssqtz@gmail.com>
2025-01-10 19:10:41 +0800

    debug

--- dl_helper/rl/rl_env/test_gym_env.py
+++ dl_helper/rl/rl_env/test_gym_env.py
@@ -1,12 +1,9 @@
 import gymnasium as gym
-try:
-    import ale_py
-    gym.register_envs(ale_py)
-except:
-    pass
+import ale_py
+gym.register_envs(ale_py)
 
 if __name__ == "__main__":
-    env = gym.make('ALE/Breakout-v5')
+    env = gym.make('ALE/Breakout-v5', obs_type='grayscale', frameskip=1)
     obs, info = env.reset()
     print(obs.shape)
     print(info)
