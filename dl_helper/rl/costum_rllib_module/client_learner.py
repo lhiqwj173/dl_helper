b10f4ea9f43ab5707c828067c9f0f2aa8a5a8f66
lhiqwj173 <lhssqtz@gmail.com>
2025-01-11 10:16:37 +0800

    debug

--- dl_helper/rl/costum_rllib_module/client_learner.py
+++ dl_helper/rl/costum_rllib_module/client_learner.py
@@ -28,7 +28,7 @@ class ClientLearnerGroup(LearnerGroup):
         """初始化客户端learner"""
         # 设置每个learner的train_title
         print(f"init_client_learner")
-        res = self.foreach_learner(lambda learner: learner.set_train_title(self.train_title))
+        res = self.foreach_learner(lambda learner: learner.set_train_title('20250108_breakout'))
         print(f"set train_title to all learners, res: {res}")
 
         # 同步参数
