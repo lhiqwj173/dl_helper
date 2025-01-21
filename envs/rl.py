3b1498b652271835978d5674461305560e0ddcdd
lhiqwj173 <lhssqtz@gmail.com>
2025-01-22 07:09:11 +0800

    debug

--- envs/rl.py
+++ envs/rl.py
@@ -25,8 +25,7 @@ if '__main__' == __name__:
     cmd = 'cd /kaggle/working/3rd && wget https://raw.githubusercontent.com/lhiqwj173/dl_helper/master/py_ext-1.0.0.tar.gz && tar -xzvf py_ext-1.0.0.tar.gz && cd py_ext-1.0.0 && python setup.py install'
     subprocess.call(cmd, shell=True)#, stdout=subprocess.DEVNULL)
 
-    for cmd in [34
-                C:\Users\lh\AppData\Local\Temp\t0_20250121.xls
+    for cmd in [
             'pip install dill',
             'pip install ale_py',
             'pip install tensorboardX',
