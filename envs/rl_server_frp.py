3bfb957ab63fe7d480ce13bcb620f22efa4bd267
lhiqwj173 <lhssqtz@gmail.com>
2025-01-25 20:41:30 +0800

    debug

---
+++ envs/rl_server_frp.py
@@ -0,0 +1,17 @@
+import subprocess
+import importlib.metadata
+import os
+import sys
+
+if '__main__' == __name__:
+    download_cmds = [
+        'wget https://raw.githubusercontent.com/lhiqwj173/dl_helper/master/envs/frpc',
+        'wget https://raw.githubusercontent.com/lhiqwj173/dl_helper/master/envs/frpc.toml',
+        'chmod +x frpc',
+    ]
+
+    for cmd in download_cmds:
+        subprocess.call(cmd, shell=True)
+
+    # frpc -c frpc.toml
+    subprocess.Popen(["frpc", "-c", "frpc.toml"], shell=True, creationflags=subprocess.DETACHED_PROCESS, close_fds=True)
