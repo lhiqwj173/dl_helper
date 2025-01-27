1bb5ce017d59097e2dbe9854aedce832b339ecf5
lhiqwj173 <lhssqtz@gmail.com>
2025-01-27 19:06:47 +0800

    debug

--- dl_helper/tests/rl/RLlib/20250110_breakout.py
+++ dl_helper/tests/rl/RLlib/20250110_breakout.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
         # 训练循环
         # rounds = 2000
         rounds = 100
-        # rounds = 5
+        rounds = 5
         for i in range(rounds):
             log(f"\nTraining iteration {i+1}/{rounds}")
             result = algo.train()
