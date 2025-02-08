1e9486cb39db10444f722cf40c2e17be57553333
lhiqwj173 <lhssqtz@gmail.com>
2025-02-08 11:08:50 +0800

    debug

--- dl_helper/rl/costum_rllib_module/client_learner.py
+++ dl_helper/rl/costum_rllib_module/client_learner.py
@@ -769,6 +769,7 @@ class ClientPPOTorchLearner(PPOTorchLearner):
             # 梯度压缩
             t = time.time()
             log(f'[{self.client_id}][{self.update_count}] compress gradients begin')
+            log(f'[{self.client_id}][{self.update_count}] need_warn_up: {self.info_data.need_warn_up}')
             compressed_grads, compress_info = self.gradient_compressor.compress(cpu_gradients, self.info_data.need_warn_up)
             cost = int((time.time() - t) * 1000)
             self.tatal_compress_cost += cost
