55dcd727f99bd52865adcc8d2fc0a92fbf31c8f9
lhiqwj173 <lhssqtz@gmail.com>
2025-03-09 17:59:52 +0800

    debug

--- dl_helper/rl/costum_rllib_module/lob/run.py
+++ dl_helper/rl/costum_rllib_module/lob/run.py
@@ -86,7 +86,7 @@ def run(
     # 验证配置
     eval_config = {
         'evaluation_interval': 15,
-        'evaluation_duration': 500,
+        'evaluation_duration': 2000,
         'evaluation_duration_unit': 'timesteps',
         'evaluation_sample_timeout_s': 24*60*60,
     }
