927542a56c39a64551a4fe6407f9b6076c8a1ceb
lhiqwj173 <lhssqtz@gmail.com>
2025-04-07 15:22:58 +0800

    debug

--- dl_helper/tests/rl/SB3/lob_trade_bc.py
+++ dl_helper/tests/rl/SB3/lob_trade_bc.py
@@ -388,7 +388,7 @@ if run_type == 'train':
         expert,
         vec_env,
         # rollout.make_sample_until(min_timesteps=500),
-        rollout.make_sample_until(min_timesteps=1.4e6),
+        rollout.make_sample_until(min_timesteps=1.35e6),
         rng=rng,
     )
     transitions = rollout.flatten_trajectories(rollouts)
