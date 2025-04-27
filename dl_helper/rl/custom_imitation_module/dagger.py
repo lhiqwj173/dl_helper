1a51715058e93d334f94d9be060cb46faac2c5df
lhiqwj173 <lhssqtz@gmail.com>
2025-04-27 08:55:20 +0800

    debug

--- dl_helper/rl/custom_imitation_module/dagger.py
+++ dl_helper/rl/custom_imitation_module/dagger.py
@@ -390,7 +390,7 @@ class SimpleDAggerTrainer(DAggerTrainer):
 
             # `logger.dump` is called inside BC.train within the following fn call:
             # 默认会训练 self.DEFAULT_N_EPOCHS(4) 个EPOCHS
-            self.extend_and_update(bc_train_kwargs)
+            # self.extend_and_update(bc_train_kwargs)
             round_num += 1
             
             # log(f"[train 3] 系统可用内存: {psutil.virtual_memory().available / (1024**3):.2f} GB")
