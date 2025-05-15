16f3499f18b3f7ecc4fd8dc71e77c028908b845a
lhiqwj173 <lhssqtz@gmail.com>
2025-05-15 18:18:21 +0800

    debug

--- envs/rl.py
+++ envs/rl.py
@@ -30,7 +30,7 @@ if '__main__' == __name__:
     for cmd in [
             'pip install --upgrade torch ray[rllib]',
             'pip install --upgrade torchvision',
-            'pip install --upgrade accelerate',
+            'pip install accelerate==1.3.0',
             'pip install imitation',
             'pip install --upgrade stable-baselines3',
             'pip install shimmy',
