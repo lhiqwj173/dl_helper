67cb5430f18a232e50988643a05acc4db336f56c
lhiqwj173 <lhssqtz@gmail.com>
2025-05-19 13:54:27 +0800

    debug

--- dl_helper/tests/bc_base/20250518_overfit_5_samples.py
+++ dl_helper/tests/bc_base/20250518_overfit_5_samples.py
@@ -267,9 +267,10 @@ class test(test_base):
 
         self.params_kwargs['y_n'] = 2
         self.params_kwargs['classify'] = True
-        self.params_kwargs['epochs'] = 10000
+        self.params_kwargs['epochs'] = 5000
         self.params_kwargs['batch_size'] = 5
         self.params_kwargs['no_better_stop'] = 0
+        self.params_kwargs['learning_rate'] = 3e-4 / 10
 
         # 实例化 参数对象
         self.para = Params(
